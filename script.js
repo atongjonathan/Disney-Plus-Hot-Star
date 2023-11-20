1b7cc1112dadfd306e664acad757bb3f46f21517
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2023-11-20 09:43:58 +0300

    improved js

--- script.js
+++ script.js
@@ -1,42 +1,37 @@
-let content_width = 1231
-function move_slider_right(elementId) {
-    let section = elementId.split("-")[0];
-    let slider_name = `${section}-slider`;
-    let slider = document.getElementById(slider_name);
-    let slider_width = slider.offsetWidth;
-
-    if (slider_width > content_width) {
-        let excess = slider_width - content_width;
-        slider.style.marginLeft = `-${excess}px`;
-    }
-};
-
-function move_slider_left(elementId)
-{
-    let section = elementId.split("-")[0];
-    let slider_name = `${section}-slider`;
-    let slider = document.getElementById(slider_name);
-    let slider_width = slider.offsetWidth;
-
-    if (slider_width > content_width) {
-        slider.style.marginLeft = `0px`;
-    }
-};
-let sliders = ["top_10", "latest", "popular", "sports","superhero", "binge"]
-for(let i=0; i<sliders.length; i++)
-{
-    let next_button = `${sliders[i]}-next`;
-    let back_button = `${sliders[i]}-back`;
-    
-    document.getElementById(next_button).addEventListener("click", function()
+document.addEventListener("DOMContentLoaded", function (){              
+    document.querySelectorAll(".button-back").forEach(function(button)
     {
-        move_slider_right(this.id);
-    })
-    document.getElementById(back_button).addEventListener("click", function()
-    {
-        move_slider_left(this.id);
-    })
-};
-
+        button.onclick = function(){
+            button_slider = button.dataset.slider
+            document.querySelectorAll('.slider').forEach(function(slider){
+                if (slider.id === button_slider){
+                    let slider_width = slider.offsetWidth;
+                    if (slider_width > 1231) {
+                            slider.style.marginLeft = `0px`;
+                        }
+                }
+            });
+            
+        };
+        
+    });
+    document.querySelectorAll(".button-next").forEach(function(button)
+        {
+            button.onclick = function(){
+                button_slider = button.dataset.slider
+                document.querySelectorAll('.slider').forEach(function(slider){
+                    if (slider.id === button_slider){
+                        let slider_width = slider.offsetWidth;
+                        if (slider_width > 1231) {
+                                let excess = slider_width - 1231;
+                                slider.style.marginLeft = `-${excess}px`;
+                            }
+                    }
+                });
+                
+            };
+            
+        });
+});
 
 
